8f3d38dfd9feaddeb8b8b060893e005e846f5f3a
L4urenmartins <lauren_martins_borba@hotmail.com>
2023-10-15 23:20:07 -0300

    Subindo o projeto funcionando a lista de tarefas

---
+++ ToDoList/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+
+import Todo from './components/ToDo';
+import TodoForm from './components/TodoForm';
+
+import "./App.css";
+
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar projeto React",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir pra academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    }
+  ]);
+
+  const addTodo = (text, category) => {
+    const newTodos = [...todos, {
+      id: Math.floor(Math.random() *10000),
+      text,
+      category,
+      isCompleted: false
+    },
+  ];
+
+  setTodos(newTodos);
+
+  const removeTodo = (id) => {
+    const newTodos = [... todos] 
+    const filteredTodos = newTodos.filter (todo => todo.id !==id ? todo : null);
+    setTodos (filteredTodos);
+  }
+
+  }
+
+  return (
+    <div className='app'>
+      <h1>Lista De Tarefas</h1>
+      <div className='todo-list'>
+        {todos.map((todo) => (
+       <Todo key={todo.id} todo={todo} removeTodo ={removeTodo} />
+        ))}
+      </div>
+      <TodoForm addTodo={addTodo} />
+    </div>
+  );
+}
+
+export default App;
+
